1d78cf24fa4977aea0bc52d685be79813a5ad8fa
amit mizrahi <amitamitamit123@gmail.com>
2018-11-03 19:08:49 +0200

    create path uses static courses data

--- src/static-data.js
+++ src/static-data.js
@@ -1,23 +1,40 @@
 export const staticCoureses = [{
-        header: "Web Devekopment",
-        subheader: "Master the languages of the web: HTML, CSS, JavaScript, and SQL. This path is great for budding front-end or back-end engineers!",
-        tags: ["html", "css", "javascript"],
-        tasks: ["html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript"]
-    },
-    {
-        header: "Data Science",
-        subheader: "Learn SQL and Python and build the skills you need to query, analyze, and visualize data.",
-        tags: ["pyhton", "sql"],
-        tasks: ["html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript"]
+    _id: 1,
+    header: "Web Devekopment",
+    subheader: "Master the languages of the web: HTML, CSS, JavaScript, and SQL. This path is great for budding front-end or back-end engineers!",
+    tags: ["html", "css", "javascript"],
+    tasks: ["html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript"]
+},
+{
+    _id: 2,
+    header: "Data Science",
+    subheader: "Learn SQL and Python and build the skills you need to query, analyze, and visualize data.",
+    tags: ["pyhton", "sql"],
+    tasks: ["html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript"]
 
+},
+{
+    _id: 3,
+    header: "Data Bases",
+    subheader: "Learn SQL and Python and build the skills you need to query, analyze, and visualize data.",
+    tags: ["pyhton", "sql"],
+    tasks: ["html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript"]
+}
+]
+export const predefinedPaths = [
+
+    {
+        name: "frontend",
+        path: [staticCoureses[0], staticCoureses[2]]
     },
     {
-        header: "Data Science",
-        subheader: "Learn SQL and Python and build the skills you need to query, analyze, and visualize data.",
-        tags: ["pyhton", "sql"],
-        tasks: ["html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript", "html", "css", "javascript"]
+        name: "backend",
+        path: [staticCoureses[1], staticCoureses[2]]
     }
+
 ]
+
+
 
 export const stats = {
     points: 200,
