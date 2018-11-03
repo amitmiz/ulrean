f964edf343601fa0c1f3312e22200c8a913f7ebc
amit mizrahi <amitamitamit123@gmail.com>
2018-11-03 19:09:20 +0200

    path predefiend

--- src/components/path-creation.jsx
+++ src/components/path-creation.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { Grid, Card, withStyles, Typography, CardHeader, CardContent, List, ListItem, ListItemText, Divider, ListItemSecondaryAction, Button, CardActions } from '@material-ui/core';
+import { Grid, Card, withStyles, Typography, CardHeader, CardContent, List, ListItem, ListItemText, Divider, ListItemSecondaryAction, Button, CardActions, IconButton, Modal, Paper, ListItemAvatar, Dialog, DialogTitle, Avatar } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add'
+import PropTypes from 'prop-types';
+import InfoIcon from '@material-ui/icons/Info'
+import { staticCoureses, predefinedPaths } from '../static-data';
+
+
 
 const styles = theme => ({
     root: {
@@ -14,7 +19,6 @@ const styles = theme => ({
     pathPreviewCircleRoot: {
         color: "black",
         display: "inline-block",
-        background: "#3f51b5",
         position: "relative",
         zIndex: 10,
 
@@ -27,7 +31,7 @@ const styles = theme => ({
             zIndex: -1,
             top: "50%",
             left: "66%",
-        
+
 
         },
         '&:last-child:after': {
@@ -63,6 +67,28 @@ class PathCreation extends React.Component {
 
     constructor(props) {
         super(props)
+
+        this.state = {
+            modalOpened: false,
+            selectedValue: null,
+            currentPath: []
+        }
+
+        this.handleOpenModal = this.handleOpenModal.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+    }
+
+    handleOpenModal() {
+        this.setState({ modalOpened: true })
+    }
+
+    handleClose(selectedValue) {
+
+        if (selectedValue && selectedValue != this.state.selectedValue) {
+            this.setState({ currentPath: [...this.state.currentPath, selectedValue] })
+        }
+
+        this.setState({ modalOpened: false, selectedValue })
     }
 
 
@@ -76,20 +102,21 @@ class PathCreation extends React.Component {
 
             <div className={classes.pathPreviewRoot} >
 
-            {["css","html"].map((name,index) =>      <this.PathCircle>{name}</this.PathCircle>)}
+                {this.state.currentPath.map((course) => <this.PathCircle>{course.header}</this.PathCircle>)}
 
+                <this.PathCircle>
+                    <IconButton onClick={this.handleOpenModal}><AddIcon /></IconButton>
+                </this.PathCircle>
 
-  
-
-
-                <this.PathCircle><   AddIcon /></this.PathCircle>
-
-
-
-
-
-
+                <SimpleDialogWrapped
+                    currentPath={this.state.currentPath}
+                    selectedValue={this.state.selectedValue}
+                    open={this.state.modalOpened}
+                    onClose={this.handleClose}
+                />
             </div>
+
+
 
         )
     }
@@ -105,25 +132,30 @@ class PathCreation extends React.Component {
             </div>
         )
     }
-    generatePredefiendList = () => (
-        <List>
-            <ListItem>
-                <ListItemText primary="Frontend" secondary="" />
-                <ListItemSecondaryAction>
-                    <Button>Use</Button>
+    generatePredefiendList = () => {
 
-                </ListItemSecondaryAction>
-            </ListItem>
-            <ListItem>
-                <ListItemText primary="Backend" secondary="" />
-                <ListItemSecondaryAction>
-                    <Button>Use</Button>
+        return (
+            <List>
 
-                </ListItemSecondaryAction>
-            </ListItem>
+                {predefinedPaths.map((predefiend) => (
+                    <ListItem>
+                        <ListItemText primary={predefiend.name} secondary="" />
+                        <ListItemSecondaryAction>
+                            <Button onClick={() => this.fillPath(predefiend)} variant="raised">Use</Button>
 
-        </List>
-    )
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                )
+
+                )}
+
+            </List>
+        )
+    }
+
+    fillPath(predefiend) {
+        this.setState({ currentPath: [...predefiend.path] })
+    }
 
     render() {
         return (
@@ -135,12 +167,8 @@ class PathCreation extends React.Component {
                         <Typography variant="h3">Create Path to Amit Mizrahi</Typography>
                     </Card>
                 </Grid>
-
-
                 <Grid item>
-
                     <Grid container direction="row" spacing={24}>
-
                         <Grid item xs={4}>
                             <Card>
                                 <CardHeader title="Predefiend paths" titleTypographyProps={{ variant: "h6" }} />
@@ -150,26 +178,17 @@ class PathCreation extends React.Component {
 
                                 </CardContent>
                             </Card>
-
                         </Grid>
-
                         <Grid item xs={8}>
-
-
-
                             <Card>
-
                                 <CardHeader title="Course Path Preview" titleTypographyProps={{ variant: "h6" }} />
                                 <Divider />
                                 <CardContent>
                                     <this.PathPreview />
-
-
-
                                 </CardContent>
                                 <CardActions>
-                                    <Button>Create</Button>
-
+                                    <Button variant="contained" >Create</Button>
+                                    <Button variant="contained" >Save as Predifiend Path</Button>
                                 </CardActions>
 
 
@@ -189,6 +208,63 @@ class PathCreation extends React.Component {
     }
 
 }
+
+const dialogStyles = {
+    avatar: {
+
+    },
+};
+
+
+class SimpleDialog extends React.Component {
+    handleClose = () => {
+        this.props.onClose(this.props.selectedValue);
+    };
+
+    handleListItemClick = value => {
+        this.props.onClose(value);
+    };
+
+    render() {
+        const { classes, onClose, selectedValue, ...other } = this.props;
+
+        const availableCourses = staticCoureses.filter((staticCourse) => {
+
+
+
+            const isFound = this.props.currentPath.find((course) => course._id === staticCourse._id)
+            return isFound == undefined;
+        })
+        const isCoursesLeft = availableCourses.length > 0
+
+        const mappedCourse = availableCourses.map(course => (
+            <ListItem button onClick={() => this.handleListItemClick(course)} key={course._id}>
+
+                <ListItemText primary={course.header} />
+                <ListItemSecondaryAction><IconButton><InfoIcon /></IconButton></ListItemSecondaryAction>
+            </ListItem>))
+
+        return (
+            <Dialog onClose={this.handleClose} {...other}>
+                <DialogTitle id="simple-dialog-title">Pick Course</DialogTitle>
+                <div>
+                    <List>
+                        {isCoursesLeft ? mappedCourse : "No coures left for selection"}
+
+                    </List>
+                </div>
+            </Dialog>
+        );
+    }
+}
+
+SimpleDialog.propTypes = {
+    classes: PropTypes.object.isRequired,
+    onClose: PropTypes.func,
+    selectedValue: PropTypes.object,
+};
+
+const SimpleDialogWrapped = withStyles(dialogStyles)(SimpleDialog);
 
 
 const styled = withStyles(styles, { withTheme: true })(PathCreation)
