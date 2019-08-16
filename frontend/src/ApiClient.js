d075c0a9cd06905e3ea00c1697bd98302cfa75ee
amit mizrahi <amitamitamit123@gmail.com>
2019-08-16 19:56:56 +0300

    added build

--- frontend/src/ApiClient.js
+++ frontend/src/ApiClient.js
@@ -5,94 +5,94 @@ import Axios from "axios";
 export class ApiClient {
 
     static register(user) {
-        return Axios.post(`http://${location.hostname}:8000/api/users`, { "user": user });
+        return Axios.post(`https://${location.hostname}/api/users`, { "user": user });
     }
 
     static login(email, password) {
-        return Axios.post(`http://${location.hostname}:8000/api/users/login`, { "user": { email, password } }, { withCredentials: true });
+        return Axios.post(`https://${location.hostname}/api/users/login`, { "user": { email, password } }, { withCredentials: true });
     }
 
     static fetchCurrUser() {
-        return Axios.get(`http://${location.hostname}:8000/api/user`, { withCredentials: true });
+        return Axios.get(`https://${location.hostname}/api/user`, { withCredentials: true });
     }
 
     static fetchUsers() {
-        return Axios.get(`http://${location.hostname}:8000/api/`);
+        return Axios.get(`https://${location.hostname}/api/`);
     }
 
     static fetchCourses() {
-        return Axios.get(`http://${location.hostname}:8000/api/courses`);
+        return Axios.get(`https://${location.hostname}/api/courses`);
     }
 
     static addCourse(course) {
-        return Axios.post(`http://${location.hostname}:8000/api/courses`, { course }, { withCredentials: true });
+        return Axios.post(`https://${location.hostname}/api/courses`, { course }, { withCredentials: true });
     }
 
     static fetchStages() {
-        return Axios.get(`http://${location.hostname}:8000/api/stages`);
+        return Axios.get(`https://${location.hostname}/api/stages`);
     }
 
     static addStage(stage) {
-        return Axios.post(`http://${location.hostname}:8000/api/stages`, { stage }, { withCredentials: true });
+        return Axios.post(`https://${location.hostname}/api/stages`, { stage }, { withCredentials: true });
     }
 
     static fetchPredefiendPaths() {
-        return Axios.get(`http://${location.hostname}:8000/api/predefiend-pathes`);
+        return Axios.get(`https://${location.hostname}/api/predefiend-pathes`);
     }
 
     static addNewPath(path) {
-        return Axios.post(`http://${location.hostname}:8000/api/predefiend-pathes`, { predefiendPath: path }, { withCredentials: true });
+        return Axios.post(`https://${location.hostname}/api/predefiend-pathes`, { predefiendPath: path }, { withCredentials: true });
     }
 
     static fetchQuestions() {
-        return Axios.get(`http://${location.hostname}:8000/api/questions`)
+        return Axios.get(`https://${location.hostname}/api/questions`)
     }
 
     static fetchQuestion(_id) {
-        return Axios.get(`http://${location.hostname}:8000/api/questions/${_id}`)
+        return Axios.get(`https://${location.hostname}/api/questions/${_id}`)
     }
 
 
     static postQuestion(question) {
-        return Axios.post(`http://${location.hostname}:8000/api/questions`, { question: question }, { withCredentials: true })
+        return Axios.post(`https://${location.hostname}/api/questions`, { question: question }, { withCredentials: true })
     }
 
     static postComment(question_id, reply) {
-        return Axios.post(`http://${location.hostname}:8000/api/questions/${question_id}/comments`, { comment: reply }, { withCredentials: true })
+        return Axios.post(`https://${location.hostname}/api/questions/${question_id}/comments`, { comment: reply }, { withCredentials: true })
     }
 
     static updateProgress({ course_id, stage }) {
-        return Axios.put(`http://${location.hostname}:8000/api/progress/${course_id}`, { courseProgress: { stagesCompleted: stage } }, { withCredentials: true })
+        return Axios.put(`https://${location.hostname}/api/progress/${course_id}`, { courseProgress: { stagesCompleted: stage } }, { withCredentials: true })
     }
 
     static updatePath({ userId, pathId }) {
-        return Axios.put(`http://${location.hostname}:8000/api/user/${userId}`, { user: { path: pathId } }, { withCredentials: true })
+        return Axios.put(`https://${location.hostname}/api/user/${userId}`, { user: { path: pathId } }, { withCredentials: true })
     }
 
 
     static updateUser({ userId, update }) {
-        return Axios.put(`http://${location.hostname}:8000/api/user/${userId}`, { user: update }, { withCredentials: true })
+        return Axios.put(`https://${location.hostname}/api/user/${userId}`, { user: update }, { withCredentials: true })
     }
 
     static logout() {
-        return Axios.post(`http://${location.hostname}:8000/api/users/logout`, null, { withCredentials: true })
+        return Axios.post(`https://${location.hostname}/api/users/logout`, null, { withCredentials: true })
     }
 
     static fetchSubmissions() {
-        return Axios.get(`http://${location.hostname}:8000/api/submissions`, { withCredentials: true })
+        return Axios.get(`https://${location.hostname}/api/submissions`, { withCredentials: true })
     }
 
 
     static fetchStageSubmissions(stage) {
-        return Axios.get(`http://${location.hostname}:8000/api/submissions/stage/${stage}`, { withCredentials: true })
+        return Axios.get(`https://${location.hostname}/api/submissions/stage/${stage}`, { withCredentials: true })
     }
 
     static postSubmission(submission) {
-        return Axios.post(`http://${location.hostname}:8000/api/submissions`, { submission }, { withCredentials: true })
+        return Axios.post(`https://${location.hostname}/api/submissions`, { submission }, { withCredentials: true })
     }
 
     static reviewSubmission(submissionsId, review) {
-        return Axios.put(`http://${location.hostname}:8000/api/submissions/${submissionsId}`, { submission: { testResult: review } }, { withCredentials: true })
+        return Axios.put(`https://${location.hostname}/api/submissions/${submissionsId}`, { submission: { testResult: review } }, { withCredentials: true })
     }
 
 
@@ -102,21 +102,21 @@ export class ApiClient {
 
 
     static usersPastDueDate() {
-        return Axios.get(`http://${location.hostname}:8000/api/stats/usersPastDueDate`).then(x => x.data)
+        return Axios.get(`https://${location.hostname}/api/stats/usersPastDueDate`).then(x => x.data)
     }
 
 
     static unfinishedPathUsers() {
-        return Axios.get(`http://${location.hostname}:8000/api/stats/unfinishedPathUsers`).then(x => x.data)
+        return Axios.get(`https://${location.hostname}/api/stats/unfinishedPathUsers`).then(x => x.data)
     }
 
 
     static finishedPathUsers() {
-        return Axios.get(`http://${location.hostname}:8000/api/stats/finishedPathUsers`).then(x => x.data)
+        return Axios.get(`https://${location.hostname}/api/stats/finishedPathUsers`).then(x => x.data)
     }
 
     static teacherStundentsCount() {
-        return Axios.get(`http://${location.hostname}:8000/api/stats/teacherStundentsCount`).then(x => x.data)
+        return Axios.get(`https://${location.hostname}/api/stats/teacherStundentsCount`).then(x => x.data)
     }
 }
 
---
+++ public/precache-manifest.f48ccd29410424d474c3896b9deda680.js
@@ -0,0 +1,354 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8d1dcd41d547609754",
+    "url": "/static/js/43.9c8d1dcd.chunk.js"
+  },
+  {
+    "revision": "9d1988595f9606442635",
+    "url": "/static/js/0.9d198859.chunk.js"
+  },
+  {
+    "revision": "c7078635098294034e14",
+    "url": "/static/js/main.c7078635.chunk.js"
+  },
+  {
+    "revision": "7cb05f31337f38666e63",
+    "url": "/static/js/2.7cb05f31.chunk.js"
+  },
+  {
+    "revision": "72b6c947ca260a912589",
+    "url": "/static/js/3.72b6c947.chunk.js"
+  },
+  {
+    "revision": "25414746df51a13e0719",
+    "url": "/static/js/4.25414746.chunk.js"
+  },
+  {
+    "revision": "88d743414712b3b58c70",
+    "url": "/static/js/5.88d74341.chunk.js"
+  },
+  {
+    "revision": "1bb29d9129a358de20a9",
+    "url": "/static/js/6.1bb29d91.chunk.js"
+  },
+  {
+    "revision": "5b0a6eaebbb080de006f",
+    "url": "/static/js/7.5b0a6eae.chunk.js"
+  },
+  {
+    "revision": "72673251588dc80167f1",
+    "url": "/static/js/8.72673251.chunk.js"
+  },
+  {
+    "revision": "622f018ad737c5b4db55",
+    "url": "/static/js/9.622f018a.chunk.js"
+  },
+  {
+    "revision": "6524f076ff89b9dc0f1a",
+    "url": "/static/js/10.6524f076.chunk.js"
+  },
+  {
+    "revision": "b245db15c81957301317",
+    "url": "/static/js/11.b245db15.chunk.js"
+  },
+  {
+    "revision": "db9f3bcffd7849f4094c",
+    "url": "/static/js/12.db9f3bcf.chunk.js"
+  },
+  {
+    "revision": "1788cd959c74a938b237",
+    "url": "/static/js/13.1788cd95.chunk.js"
+  },
+  {
+    "revision": "4ab20f640ee2eb63fcd0",
+    "url": "/static/js/14.4ab20f64.chunk.js"
+  },
+  {
+    "revision": "db0582efadb452f4bfff",
+    "url": "/static/js/15.db0582ef.chunk.js"
+  },
+  {
+    "revision": "3b6c3b87625d222ef1bc",
+    "url": "/static/js/16.3b6c3b87.chunk.js"
+  },
+  {
+    "revision": "bf41de1ae7a98796c7ce",
+    "url": "/static/js/17.bf41de1a.chunk.js"
+  },
+  {
+    "revision": "d7de9dbb3ef79cda0383",
+    "url": "/static/js/18.d7de9dbb.chunk.js"
+  },
+  {
+    "revision": "cdca137f7bedcdaca30e",
+    "url": "/static/js/19.cdca137f.chunk.js"
+  },
+  {
+    "revision": "3c8961cc692f589765da",
+    "url": "/static/js/20.3c8961cc.chunk.js"
+  },
+  {
+    "revision": "5c8417c15d1ca084de35",
+    "url": "/static/js/21.5c8417c1.chunk.js"
+  },
+  {
+    "revision": "2f36b6ffdd350d6ebe1d",
+    "url": "/static/js/22.2f36b6ff.chunk.js"
+  },
+  {
+    "revision": "e747ee01abc956fff5d7",
+    "url": "/static/js/23.e747ee01.chunk.js"
+  },
+  {
+    "revision": "03230e435b8812803749",
+    "url": "/static/js/24.03230e43.chunk.js"
+  },
+  {
+    "revision": "6ab0333094b9e1189ed7",
+    "url": "/static/js/25.6ab03330.chunk.js"
+  },
+  {
+    "revision": "deb2df9e1103e40a08d0",
+    "url": "/static/js/26.deb2df9e.chunk.js"
+  },
+  {
+    "revision": "071470e9de88ac70839f",
+    "url": "/static/js/27.071470e9.chunk.js"
+  },
+  {
+    "revision": "f6ea98d4b051089c7811",
+    "url": "/static/js/28.f6ea98d4.chunk.js"
+  },
+  {
+    "revision": "2ce9db6544c9c5446764",
+    "url": "/static/js/29.2ce9db65.chunk.js"
+  },
+  {
+    "revision": "55a2600cf4c6b26ed634",
+    "url": "/static/js/30.55a2600c.chunk.js"
+  },
+  {
+    "revision": "09746c2225c11e833c9c",
+    "url": "/static/js/31.09746c22.chunk.js"
+  },
+  {
+    "revision": "c367d4cea59eb90db5cf",
+    "url": "/static/js/32.c367d4ce.chunk.js"
+  },
+  {
+    "revision": "e4ea1db12372405d1296",
+    "url": "/static/js/33.e4ea1db1.chunk.js"
+  },
+  {
+    "revision": "2ca01d09ee6e51ffdcd1",
+    "url": "/static/js/34.2ca01d09.chunk.js"
+  },
+  {
+    "revision": "ebfa7f7169a5f4e3f4de",
+    "url": "/static/js/35.ebfa7f71.chunk.js"
+  },
+  {
+    "revision": "87bec9c962e3dd1f59fb",
+    "url": "/static/js/36.87bec9c9.chunk.js"
+  },
+  {
+    "revision": "c6e47730ef84faefe700",
+    "url": "/static/js/37.c6e47730.chunk.js"
+  },
+  {
+    "revision": "19b35334b35b74d27b9e",
+    "url": "/static/js/38.19b35334.chunk.js"
+  },
+  {
+    "revision": "deea37dcf23cd741d27f",
+    "url": "/static/js/39.deea37dc.chunk.js"
+  },
+  {
+    "revision": "fe93ce69f90d5cb099e3",
+    "url": "/static/js/40.fe93ce69.chunk.js"
+  },
+  {
+    "revision": "b8de1995774a7190eb74",
+    "url": "/static/js/41.b8de1995.chunk.js"
+  },
+  {
+    "revision": "b86aef0ab2126ca99506",
+    "url": "/static/js/42.b86aef0a.chunk.js"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/static/media/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "190e4015bb3a5d3a42e6",
+    "url": "/static/js/44.190e4015.chunk.js"
+  },
+  {
+    "revision": "949a2cd04fe149c8de83",
+    "url": "/static/js/45.949a2cd0.chunk.js"
+  },
+  {
+    "revision": "673e3730c6a6db36391d",
+    "url": "/static/js/46.673e3730.chunk.js"
+  },
+  {
+    "revision": "f70f44d9e4cd7cd019c0",
+    "url": "/static/js/47.f70f44d9.chunk.js"
+  },
+  {
+    "revision": "dc08308b4c819c109996",
+    "url": "/static/js/48.dc08308b.chunk.js"
+  },
+  {
+    "revision": "f2af3aacfcbd46e655bb",
+    "url": "/static/js/49.f2af3aac.chunk.js"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "4be47c733254616ec45f",
+    "url": "/static/js/50.4be47c73.chunk.js"
+  },
+  {
+    "revision": "57fb834d5b71d0cd37ad",
+    "url": "/static/js/51.57fb834d.chunk.js"
+  },
+  {
+    "revision": "77996390055de9d70736",
+    "url": "/static/js/52.77996390.chunk.js"
+  },
+  {
+    "revision": "b95002bc641cba8b0cf3",
+    "url": "/static/js/53.b95002bc.chunk.js"
+  },
+  {
+    "revision": "983dfc316e3d101ed279",
+    "url": "/static/js/54.983dfc31.chunk.js"
+  },
+  {
+    "revision": "0e75380af3d0ca52b86e",
+    "url": "/static/js/runtime~main.0e75380a.js"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/static/media/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "3d5874be332f0e06a761333aa161d027",
+    "url": "/typescript.worker.js"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/static/media/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/static/media/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "c7078635098294034e14",
+    "url": "/static/css/main.28b42843.chunk.css"
+  },
+  {
+    "revision": "4be47c733254616ec45f",
+    "url": "/static/css/50.dbfce4d1.chunk.css"
+  },
+  {
+    "revision": "4fdad9a34e32cc3fea51ebc07cb76a28",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "16b9ecc728035da94f8b3cb540168e26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b7e9951cd378ae04548ad78072ebb1",
+    "url": "/html.worker.js"
+  },
+  {
+    "revision": "9e805a27169c19b228b759942e63d3b4",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "b5f3bd04f3601f7d974b61d545a69887",
+    "url": "/css.worker.js"
+  }
+];
